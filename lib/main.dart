331f6b49629432802d1ad5fc955e60cb82437e2c
canoe726 <canoe918@gmail.com>
2020-02-05 20:54:37 +0900

    Make Stateful Widget & use Lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: YBCard(),
+  home: QuoteList(),
 ));
 
-class YBCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _YBCardState createState() => _YBCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _YBCardState extends State<YBCard> {
+class _QuoteListState extends State<QuoteList> {
 
-  int ybLevel = 0;
+  List<String> quotes = [
+    "Be yourself; everyone else is already taken",
+    "I have nothing to declare except my genius",
+    "The truth is rarely pure and never simple",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo[300],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('YB ID Card'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.indigo,
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() { // trigger of build function
-            ybLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.indigoAccent,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/canoe.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.white,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'YoungBae',
-              style: TextStyle(
-                color: Colors.blueGrey[900],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30),
-            Text(
-              'CURRENT YB LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '$ybLevel',
-              style: TextStyle(
-                color: Colors.blueGrey[900],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'YoungBae@gmail.com',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize:18.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
     );
   }
 }
-
-
 
