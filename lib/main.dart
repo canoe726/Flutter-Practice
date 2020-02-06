e2ec75c326b8f3f94bcf9048aefb5cd0b5260b50
canoe726 <canoe918@gmail.com>
2020-02-06 23:23:29 +0900

    Use Maps & Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,13 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:flutter_app/pages/home.dart';
+import 'package:flutter_app/pages/choose_location.dart';
+import 'package:flutter_app/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: QuoteList(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(author:'Oscar wilde', text: "Be yourself; everyone else is already taken"),
-    Quote(author:'Conan', text: "I have nothing to declare except my genius"),
-    Quote(author:'Kim', text: "The truth is rarely pure and never simple"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: () {
-            setState(() {
-              quotes.remove(quote);
-            });
-          }
-        )).toList(),
-      ),
-    );
-  }
-}
-
-
