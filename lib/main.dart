59d287ab30f152035fcb22b6be3e61097106cebf
canoe726 <canoe918@gmail.com>
2020-02-07 13:26:26 +0900

    Using Flutter (http) Packages

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_app/pages/choose_location.dart';
 import 'package:flutter_app/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/home',
+  initialRoute: '/',
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
